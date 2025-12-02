a1a71fe9796e24d4f9d39c95e9647bc83b007033
AlejoGDOfficial <alejogdoff@gmail.com>
2025-12-02 18:01:35 -0500

    da libs

---
+++ setup/install-haxelibs.bat
@@ -0,0 +1,28 @@
+@echo off
+cd ..
+@echo on
+echo Installing dependencies
+
+@if not exist ".haxelib\" mkdir .haxelib
+
+echo Installing dependencies...
+
+haxelib install hxcpp 4.3.2
+
+haxelib install lime 7.9.0
+
+haxelib install openfl 9.1.0 --skip-dependencies
+
+haxelib install flixel 4.11.0 --skip-dependencies
+haxelib install flixel-addons 2.11.0 --skip-dependencies
+haxelib install flixel-ui 2.5.0 --skip-dependencies
+
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc 2d83fa863ef0c1eace5f1cf67c3ac315d1a3a8a5 --skip-dependencies
+
+haxelib install hscript 2.5.0
+
+haxelib install polymod 1.3.0
+
+echo Finished!
+
+pause
---
+++ setup/install-haxelibs.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+cd ..
+
+mkdir ~/haxelib && haxelib setup ~/haxelib
+
+echo "Installing dependencies..."
+
+haxelib install hxcpp 4.3.2
+
+haxelib install lime 7.9.0
+
+haxelib install openfl 9.1.0 --skip-dependencies
+
+haxelib install flixel 4.11.0 --skip-dependencies
+haxelib install flixel-addons 2.11.0 --skip-dependencies
+haxelib install flixel-ui 2.5.0 --skip-dependencies
+
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc 2d83fa863ef0c1eace5f1cf67c3ac315d1a3a8a5 --skip-dependencies
+
+haxelib install hscript 2.5.0
+
+haxelib install polymod 1.3.0
+
+echo "Finished!"
